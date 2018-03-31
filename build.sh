c80f3af9c08abe50785190e1a81a7cf2fd21345e
giasen <giammasena@gmail.com>
2018-03-31 22:34:03 +0530

    script for building kernel

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CCACHE=$(command -v ccache)
+
+TOOLCHAIN=/home/giasen/du/prebuilts/gcc/linux-x86/aarch64/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+
+export CROSS_COMPILE="${CCACHE} ${TOOLCHAIN}"
+
+export ARCH=arm64
+
+make clean O=out/
+make mrproper O=out/
+
+make mido_defconfig O=out/
+
+make -j8 O=out/
